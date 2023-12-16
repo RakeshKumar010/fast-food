f2a4e36ae4d00422dcd61b0461373d5a5242888f
Rakesh Kumar <raksehkumar748844@gmail.com>
2023-12-16 17:23:06 +0530

    add new item section

--- src/components/orders/CustomizeItem.jsx
+++ src/components/orders/CustomizeItem.jsx
@@ -5,6 +5,8 @@ import Veggie from "./Customize/Veggie";
 const CustomizeItem = () => {
   const [veggie, setVeggie] = useState(true);
   const [meat, setMeat] = useState(false);
+  const [cheese, setCheese] = useState(false);
+  const [sauce, setSauce] = useState(false);
 
   const veggieItems = [
     "ALMONDS $(0.99)",
@@ -49,33 +51,70 @@ const CustomizeItem = () => {
     "SALAMI $(1.50)",
     "SHAVED STEAK $(1.50)",
   ];
-
+  const cheeseItem = [
+    "CHEDDAR CHEESE $(1.50)",
+    "CHEESE",
+    "FETA CHEESE $(1.50)",
+    "GREEK FETA $(1.50)",
+    "MOZZA $(1.50)",
+    "PARMESAN CHEESE $(1.50)",
+  ];
+  const sauceItem = [
+    "BBQ SAUCE $(0.99)",
+    "BUFFALO SAUCE $(0.99)",
+    "GARLIC SAUCE $(0.99)",
+    "HONEY GARLIC SAUCE $(0.99)",
+    "RANCH SAUCE $(0.99)",
+  ];
   return (
     <>
       <div className="lg:w-1/2 w-full lg:h-3/4 overflow-y-scroll bg-gray-50 rounded-lg shadow-sm-light shadow-gray-500 relative">
         <div className="flex p-4 gap-6 border-b-2 border-black">
           <Link
             onClick={() => {
-              setVeggie(!veggie);
+              setVeggie(true);
+              setMeat(false);
+              
+              setCheese(false);
+            setSauce(false)
             }}
           >
             VEGGIE
           </Link>
           <Link
             onClick={() => {
-              setMeat(!meat);
-              setVeggie(!veggie)
+              setMeat(true);
+              setVeggie(false);
+              setCheese(false);
+            setSauce(false)
+
             }}
           >
             MEAT
           </Link>
-          <Link>CHEESE</Link>
-          <Link>SAUCE</Link>
+          <Link
+            onClick={() => {
+              setCheese(true);
+              setVeggie(false);
+              setMeat(false);
+            setSauce(false)
+
+            }}
+          >
+            CHEESE
+          </Link>
+          <Link onClick={()=>{
+            setSauce(true)
+            setCheese(false);
+            setVeggie(false);
+            setMeat(false);
+          }}>SAUCE</Link>
         </div>
         <div className="p-2">
-            {veggie?<Veggie items={veggieItems} />:null}
-            {meat?<Veggie items={meatItems} />:null}
-          
+          {veggie ? <Veggie items={veggieItems} /> : null}
+          {meat ? <Veggie items={meatItems} /> : null}
+          {cheese ? <Veggie items={cheeseItem} /> : null}
+          {sauce ? <Veggie items={sauceItem } /> : null}
         </div>
       </div>
     </>
